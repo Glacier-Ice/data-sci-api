4d4b93cd9b735e5a8587d53215855c14f49e77c7
Vopaaz <liyifan945@163.com>
2020-02-16 13:55:02 +0800

    Add readme

--- src-etl/point-to-point-migration/dateutils.py
+++ src-etl/point-to-point-migration/dateutils.py
@@ -11,13 +11,13 @@ def yesterday():
     return (datetime.date.today() + datetime.timedelta(days=-1)).strftime(DATE_FORMAT)
 
 
-def all_days_since_last_month(end_date: str):
-    end_date = datetime.datetime.strptime(end_date, DATE_FORMAT).date()
-    to_add = end_date + datetime.timedelta(days=-30)
+def all_days_since_last_month(end_date_not_inclusive: str):
+    end_date_not_inclusive = datetime.datetime.strptime(end_date_not_inclusive, DATE_FORMAT).date()
+    to_add = end_date_not_inclusive + datetime.timedelta(days=-30)
     res = []
     while True:
         res.append(to_add.strftime(DATE_FORMAT))
         to_add += datetime.timedelta(days=1)
-        if to_add == end_date:
+        if to_add == end_date_not_inclusive:
             break
     return res

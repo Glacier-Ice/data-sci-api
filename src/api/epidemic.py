98d3694ee29d0de88095729801a9edd42fb913be
Rex Wang <rexwangcc@gmail.com>
2020-02-21 17:26:02 -0500

    Implement Query endpoint handler.

--- src/api/epidemic.py
+++ src/api/epidemic.py
@@ -1,11 +1,15 @@
 from src.core import database
+import datetime
+
+
+DATE_FORMAT: str = "%Y-%m-%d"
 
 
 def deserilize_edpidemic_city(record: tuple) -> dict:
     """Deserilize the DB records to API formats for by city data."""
     return {
         "id": int(record[0]),
-        "curday": record[1].strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+        "curday": record[1].strftime(DATE_FORMAT),
         "city_name": str(record[2]),
         "city_code": str(record[3]),
         "confirmed_count": int(record[4]),
@@ -19,7 +23,7 @@ def deserilize_edpidemic_province(record: tuple) -> dict:
     """Deserilize the DB records to API formats for by province data."""
     return {
         "id": int(record[0]),
-        "curday": record[1].strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+        "curday": record[1].strftime(DATE_FORMAT),
         "province_name": str(record[2]),
         "province_code": str(record[3]),
         "confirmed_count": int(record[4]),
@@ -41,8 +45,8 @@ def get_all(granularity: str, limit: int = 0) -> list:
     if limit <= 0:
         limit = None
 
-    SQL = """SELECT * FROM %(table) 
-             LIMIT %(limit)"""
+    # table name is not transormbale by psycopg2
+    SQL = """SELECT * FROM {table} LIMIT %(limit)s""".format(table=table)
 
     results = database.query(sql=SQL, table=table, limit=limit)
     return list(map(deserilize_func, results))
@@ -58,16 +62,22 @@ def query(name: list, granularity: str, start: str = None, end: str = None, limi
         deserilize_func = deserilize_edpidemic_city
 
     if start:
-        pass
+        start = datetime.datetime.strptime(start, DATE_FORMAT)
+
+    if end:
+        end = datetime.datetime.strptime(end, DATE_FORMAT)
+
     if limit <= 0:
         limit = None
 
-    SQL = """SELECT * 
-             FROM %(table) 
-             WHERE
-                (%(start)s is null or %(start)s = age)
-                AND
-                (%(age)s is null or %(age)s = age)
-             LIMIT %(limit)"""
-    results = database.query(sql=SQL, table=table, limit=limit, start=start, end=end)
+    # table name is not transormbale by psycopg2
+    SQL = """SELECT * FROM {table} AS t 
+             WHERE 
+             (%(start)s is null or %(start)s <= t.curday) 
+             AND 
+             (%(end)s is null or %(end)s >= t.curday) 
+             LIMIT %(limit)s""".format(
+        table=table
+    )
+    results = database.query(sql=SQL, limit=limit, start=start, end=end)
     return list(map(deserilize_func, results))

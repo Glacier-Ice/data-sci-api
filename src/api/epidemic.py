870017c8628c50bd3e6c5f3a85113e7fa9186f91
Rex Wang <rexwangcc@gmail.com>
2020-02-21 00:27:43 -0500

    Checkpoint.

--- src/api/epidemic.py
+++ src/api/epidemic.py
@@ -29,22 +29,45 @@ def deserilize_edpidemic_province(record: tuple) -> dict:
     }
 
 
-def get_all(granularity: str, limit: int = 0):
+def get_all(granularity: str, limit: int = 0) -> list:
     """Fetch all of the available epidemic data based on GRANULARITY and LIMIT."""
     if granularity == "province":
-        TABLE = "area_province"
+        table = "area_province"
         deserilize_func = deserilize_edpidemic_province
     else:
-        TABLE = "area_city"
+        table = "area_city"
         deserilize_func = deserilize_edpidemic_city
 
-    SQL = f"SELECT * FROM {TABLE}"
-    if limit > 0:
-        SQL += f" LIMIT {limit}"
+    if limit <= 0:
+        limit = None
 
-    results = database.query(sql=SQL)
+    SQL = """SELECT * FROM %(table) 
+             LIMIT %(limit)"""
+
+    results = database.query(sql=SQL, table=table, limit=limit)
     return list(map(deserilize_func, results))
 
 
-def query():
-    pass
+def query(name: list, granularity: str, start: str = None, end: str = None, limit: int = 0) -> list:
+    """Get epidemic data by specific criterias."""
+    if granularity == "province":
+        table = "area_province"
+        deserilize_func = deserilize_edpidemic_province
+    else:
+        table = "area_city"
+        deserilize_func = deserilize_edpidemic_city
+
+    if start:
+        pass
+    if limit <= 0:
+        limit = None
+
+    SQL = """SELECT * 
+             FROM %(table) 
+             WHERE
+                (%(start)s is null or %(start)s = age)
+                AND
+                (%(age)s is null or %(age)s = age)
+             LIMIT %(limit)"""
+    results = database.query(sql=SQL, table=table, limit=limit, start=start, end=end)
+    return list(map(deserilize_func, results))
